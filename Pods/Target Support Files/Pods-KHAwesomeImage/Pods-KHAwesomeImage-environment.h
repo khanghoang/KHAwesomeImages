45833ed8889ab66a4bea9d1418ecc56e4e87400f
khanghoang <hoangtrieukhang@gmail.com>
2014-10-08 13:01:48 +0700

    Pods

---
+++ Pods/Target Support Files/Pods-KHAwesomeImage/Pods-KHAwesomeImage-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// 500px-iOS-api
+#define COCOAPODS_POD_AVAILABLE_500px_iOS_api
+#define COCOAPODS_VERSION_MAJOR_500px_iOS_api 1
+#define COCOAPODS_VERSION_MINOR_500px_iOS_api 0
+#define COCOAPODS_VERSION_PATCH_500px_iOS_api 5
+
+// LBDelegateMatrioska
+#define COCOAPODS_POD_AVAILABLE_LBDelegateMatrioska
+#define COCOAPODS_VERSION_MAJOR_LBDelegateMatrioska 0
+#define COCOAPODS_VERSION_MINOR_LBDelegateMatrioska 0
+#define COCOAPODS_VERSION_PATCH_LBDelegateMatrioska 2
+
