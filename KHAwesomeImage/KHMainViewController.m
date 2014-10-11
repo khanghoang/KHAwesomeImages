6ab8b350991be8604e4cb3bfff00eff6710f88d7
khanghoang <hoangtrieukhang@gmail.com>
2014-10-11 15:36:51 +0700

    Add main vc

---
+++ KHAwesomeImage/KHMainViewController.m
@@ -0,0 +1,37 @@
+//
+//  KHMainViewController.m
+//  KHAwesomeImage
+//
+//  Created by Triệu Khang on 11/10/14.
+//  Copyright (c) 2014 Triệu Khang. All rights reserved.
+//
+
+#import "KHMainViewController.h"
+
+@interface KHMainViewController ()
+
+@end
+
+@implementation KHMainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
