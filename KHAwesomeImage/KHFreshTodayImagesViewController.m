1e042223deb47ebca0047d275844fa823f84657e
khanghoang <hoangtrieukhang@gmail.com>
2014-10-11 21:39:32 +0700

    Add fresh images storyboard & vc

---
+++ KHAwesomeImage/KHFreshTodayImagesViewController.m
@@ -0,0 +1,24 @@
+//
+//  KHFreshTodayImagesViewController.m
+//  KHAwesomeImage
+//
+//  Created by Triệu Khang on 11/10/14.
+//  Copyright (c) 2014 Triệu Khang. All rights reserved.
+//
+
+#import "KHFreshTodayImagesViewController.h"
+
+@interface KHFreshTodayImagesViewController ()
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation KHFreshTodayImagesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
