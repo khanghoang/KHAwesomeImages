38e4ad05bbe726987dfbe656a72b4d6b1cf89436
khanghoang <hoangtrieukhang@gmail.com>
2014-10-11 15:58:27 +0700

    finish tab 1

--- KHAwesomeImage/KHMainViewController.m
+++ KHAwesomeImage/KHMainViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "KHMainViewController.h"
+#import "ViewController.h"
 
 typedef NS_ENUM(NSUInteger, kMainViewControllerSegmentValue) {
     kMainViewControllerSegmentValuePopular = 0,
@@ -32,7 +33,11 @@ typedef NS_ENUM(NSUInteger, kMainViewControllerSegmentValue) {
 }
 
 - (void)changeChildViewControllerWithType:(kMainViewControllerSegmentValue)type {
-
+    ViewController *vc = [[UIStoryboard storyboardWithName:@"Popular" bundle:nil] instantiateViewControllerWithIdentifier:NSStringFromClass([ViewController class])];
+    [vc willMoveToParentViewController:self];
+    [self.view addSubview:vc.view];
+    [self addChildViewController:vc];
+    [vc didMoveToParentViewController:self];
 }
 
 @end
